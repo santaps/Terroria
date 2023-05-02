8c5521a6d5d1213bbf036d54ac0a4ee1c16bb9c3
santaps <santaps@users.noreply.github.com>
2023-05-02 11:28:18 +0300

    Custom laser base

---
+++ Projectiles/DuckLaserProjectile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace StarterMod.Projectiles
+{
+    public class DuckLaserProjectile : ModProjectile
+    {
+
+        public override string Texture => $"Terraria/Images/Item_{ItemID.FallenStar}";
+
+        // The actual distance is stored in the ai0 field
+        // By making a property to handle this it makes our life easier, and the accessibility more readable
+        public float Distance
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Quacking large laser");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 10; // The width of projectile hitbox
+            Projectile.height = 10; // The height of projectile hitbox
+
+            Projectile.aiStyle = 0; // The ai style of the projectile (0 means custom AI). For more please reference the source code of Terraria
+            Projectile.DamageType = DamageClass.Magic; // What type of damage does this projectile affect?
+            Projectile.friendly = false; // Can the projectile deal damage to enemies?
+            Projectile.hostile = true; // Can the projectile deal damage to the player?
+            Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
+            Projectile.light = 1f; // How much light emit around the projectile
+            Projectile.tileCollide = false; // Can the projectile collide with tiles?
+            Projectile.timeLeft = 720; // The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+        }
+
+        public override void AI()
+        {
+
+        }
+    }
+}
